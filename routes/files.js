7cf4f8ee9725dbbebe0c958ce9f7cec9fb8a1ea2
iceking-fct <miracleamajama14@gmail.com>
2025-01-07 16:02:05 +0100

    all tasks under the routes/ directory solved

---
+++ routes/files.js
@@ -0,0 +1,16 @@
+import { Router, json } from 'express';
+import {
+  fileUpload, getFile,
+  getIndex, getShow,
+  putPubUnpulish,
+} from '../controllers';
+
+const router = Router();
+router.use(json({ limit: '2mb' }));
+router.post('/', fileUpload);
+router.get('/', getIndex);
+router.get('/:id', getShow);
+router.put(['/:id/publish', '/:id/unpublish'], putPubUnpulish);
+router.get('/:id/data', getFile);
+
+export default router;
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import filesRouter from './files';
+import appRouter from './app';
+import usersRouter from './users';
+import authRouter from './auth';
+
+export {
+  authRouter,
+  appRouter,
+  filesRouter,
+  usersRouter,
+};
